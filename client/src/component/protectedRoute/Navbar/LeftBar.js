980341179d468f06cd986b65d76fe36ed912be36
CK <wongchuankai97@gmail.com>
2021-10-12 20:33:52 +0800

    fix leftbar responsive ui

--- client/src/component/protectedRoute/Navbar/LeftBar.js
+++ client/src/component/protectedRoute/Navbar/LeftBar.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
-import { Container, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material'
+// import { Container, Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Divider } from '@mui/material'
 import Typography from '@mui/material/Typography';
 import { makeStyles } from '@mui/styles'
+import {Nav} from 'react-bootstrap'
 import { Home, People, Chat, Description } from "@mui/icons-material";
 
 // import InboxIcon from '@mui/icons-material/Inbox';
@@ -21,6 +22,8 @@ const useStyles = makeStyles((theme) => ({
         left: 0,
         bottom: 0,
         overflow: 'hidden',
+        display:"flex",
+        flexDirection: "column",
         [theme.breakpoints.up("sm")]: {
             backgroundColor: "#F5F5F5",
             color: "#555",
@@ -67,45 +70,55 @@ function LeftBar() {
         
     }
     return (
-        <Box className={classes.container}>
-            <nav aria-label="main left navbar">
-                <List>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={goToHomeHandler}>
-                        <ListItemIcon>
-                            <Home/>
-                        </ListItemIcon>
-                        <ListItemText primary="Home" className={classes.tabsIcon}/>
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={goToFriendsListHandler}>
-                        <ListItemIcon>
-                            <People/>
-                        </ListItemIcon>
-                        <ListItemText primary="Friends List" className={classes.tabsIcon}/>
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={goToChatRoomHandler}>
-                        <ListItemIcon>
-                            <Chat/>
-                        </ListItemIcon>
-                        <ListItemText primary="Chat Room" className={classes.tabsIcon}/>
-                    </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                    <ListItemButton onClick={goToUserGuideHandler}>
-                        <ListItemIcon>
-                            <Description/>
-                        </ListItemIcon>
-                        <ListItemText primary="User Guide" className={classes.tabsIcon}/>
-                    </ListItemButton>
-                </ListItem>
-                </List>
-            </nav>
-            <Divider />
-        </Box>
+        <Nav defaultActiveKey="/home" className={classes.container}>
+        <Nav.Link href="/home">Active</Nav.Link>
+        <Nav.Link eventKey="link-1">Link</Nav.Link>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+        <Nav.Link eventKey="disabled" disabled>
+            Disabled
+        </Nav.Link>
+        </Nav>
+        // <Box className={classes.container}>
+        //     <nav aria-label="main left navbar">
+        //         <List>
+        //         <ListItem disablePadding>
+        //             <ListItemButton onClick={goToHomeHandler}>
+        //                 <ListItemIcon>
+        //                     <Home/>
+        //                 </ListItemIcon>
+        //                 <ListItemText primary="Home" className={classes.tabsIcon}/>
+        //             </ListItemButton>
+        //         </ListItem>
+        //         <ListItem disablePadding>
+        //             <ListItemButton onClick={goToFriendsListHandler}>
+        //                 <ListItemIcon>
+        //                     <People/>
+        //                 </ListItemIcon>
+        //                 <ListItemText primary="Friends List" className={classes.tabsIcon}/>
+        //             </ListItemButton>
+        //         </ListItem>
+        //         <ListItem disablePadding>
+        //             <ListItemButton onClick={goToChatRoomHandler}>
+        //                 <ListItemIcon>
+        //                     <Chat/>
+        //                 </ListItemIcon>
+        //                 <ListItemText primary="Chat Room" className={classes.tabsIcon}/>
+        //             </ListItemButton>
+        //         </ListItem>
+        //         <ListItem disablePadding>
+        //             <ListItemButton onClick={goToUserGuideHandler}>
+        //                 <ListItemIcon>
+        //                     <Description/>
+        //                 </ListItemIcon>
+        //                 <ListItemText primary="User Guide" className={classes.tabsIcon}/>
+        //             </ListItemButton>
+        //         </ListItem>
+        //         </List>
+        //     </nav>
+        //     <Divider />
+        // </Box>
+
+
         // <Container className={classes.container} >
 
         //     {/* <div className={classes.tabs}>
